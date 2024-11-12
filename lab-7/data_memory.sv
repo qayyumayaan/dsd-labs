46c14dd43f12fc2408552428bdfcde86874a4687
Ayaan Qayyum <96453808+qayyumayaan@users.noreply.github.com>
2024-11-12 17:57:57 -0500

    Update data_memory.sv

--- lab-7/data_memory.sv
+++ lab-7/data_memory.sv
@@ -1,20 +1,12 @@
 module data_memory(
     input logic clk, rst,
-    input logic [31:0] A,    // address
-    input logic [31:0] WD,   // input data
-    input logic WE,          // enable input
-    output logic [31:0] RD   // output data
-    output logic [31:0] probe // Data memory probe
+    input logic [31:0] A,     // address
+    input logic [31:0] WD,    // input data
+    input logic WE,           // enable input
+    output logic [31:0] RD,   // output data
+	output logic [31:0] probe // Data memory probe
 );
     logic [31:0] memory [0:255]; // 256 words of 32-bit memory
-
-    // // Initialize memory
-    // initial begin
-    //     integer i;
-    //     for (i = 0; i < 256; i = i + 1) begin
-    //         memory[i] = 32'b0;
-    //     end
-    // end
 
     // Write operation
     always_ff @(posedge clk or negedge rst) begin
