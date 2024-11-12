480521b7e5c4220584584dde2db13fcdf09e77fd
Ayaan Qayyum <96453808+qayyumayaan@users.noreply.github.com>
2024-11-12 10:07:13 -0500

    Rename ReadData2 as RD2 for uniformity

--- lab-7/MUX_ALUSrc.sv
+++ lab-7/MUX_ALUSrc.sv
@@ -1,8 +1,8 @@
 module MUX_ALUSrc(
     input logic ALUSrc,
-    input logic [31:0] ReadData2,  // from Register File
+    input logic [31:0] RD2,  // from Register File
     input logic [31:0] SignImm,    // sign-extended immediate
     output logic [31:0] ALUSrc_out
 );
-    assign ALUSrc_out = ALUSrc ? SignImm : ReadData2;
+    assign ALUSrc_out = ALUSrc ? SignImm : RD2;
 endmodule
